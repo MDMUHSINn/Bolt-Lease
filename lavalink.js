f720baa83afa093dcacd5dc83c8aa6666237e143
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2025-12-06 04:05:09 -0800

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "GlaceYT",
+      password: "glace",
+      host: "us-01.strixnodes.com",
+      port:  8003,
+      secure: false
+    }
+};           
+
